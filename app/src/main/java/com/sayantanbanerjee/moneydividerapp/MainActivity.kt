cd4703dafa4116be00537ea2216cc3f27c339b8d
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-07-01 13:00:10 +0530

    View model implemented

--- app/src/main/java/com/sayantanbanerjee/moneydividerapp/MainActivity.kt
+++ app/src/main/java/com/sayantanbanerjee/moneydividerapp/MainActivity.kt
@@ -6,39 +6,38 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.sayantanbanerjee.moneydividerapp.databinding.ActivityMainBinding
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var amount1numberText: EditText
-    private lateinit var amount2numberText: EditText
-    private lateinit var display: TextView
-    private lateinit var calculate: Button
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        amount1numberText = findViewById(R.id.person1amount)
-        amount2numberText = findViewById(R.id.person2amount)
-        display = findViewById(R.id.display)
-        calculate = findViewById(R.id.calculate)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
-        calculate.setOnClickListener {
-            val amount1 = amount1numberText.text.toString()
-            val amount2 = amount2numberText.text.toString()
-            if (amount1 == "" || amount2 == "") {
-                Toast.makeText(this, "No field should remain empty", Toast.LENGTH_SHORT).show()
-            } else {
-                if (amount1 == amount2) {
-                    display.text = getString(R.string.amountEqual)
-                } else if (amount1 > amount2) {
-                    val diff = amount1.toInt() - amount2.toInt()
-                    display.text = getString(R.string.amount1more) + " " + diff.toString()
-                } else {
-                    val diff = amount2.toInt() - amount1.toInt()
-                    display.text = getString(R.string.amount2more) + " " + diff.toString()
-                }
+        viewModel.noValueToast.observe(this, Observer {
+            Toast.makeText(this, "No field should remain empty", Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.difference.observe(this , Observer {
+            if(it == 0){
+                binding.display.text = getString(R.string.amountEqual)
+            }else if(it > 0){
+                binding.display.text = getString(R.string.amount1more) + " " + it.toString()
+            }else{
+                binding.display.text = getString(R.string.amount2more) + " " + it.absoluteValue.toString()
             }
-        }
+        })
     }
 }
---
+++ app/src/main/java/com/sayantanbanerjee/moneydividerapp/MainActivityViewModel.kt
@@ -0,0 +1,26 @@
+package com.sayantanbanerjee.moneydividerapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    val amountPerson1 = MutableLiveData<String>()
+    val amountPerson2 = MutableLiveData<String>()
+
+    private var noValue = MutableLiveData<String>()
+    val noValueToast: LiveData<String>
+        get() = noValue
+
+    private var diff = MutableLiveData<Int>()
+    val difference: LiveData<Int>
+        get() = diff
+
+    fun calculate(){
+        if (amountPerson1.value == "" || amountPerson2.value == "") {
+            noValue.value = "NONE"
+        } else {
+             diff.value = amountPerson1.value!!.toInt().minus(amountPerson2.value!!.toInt())
+        }
+    }
+}
